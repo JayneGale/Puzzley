31093affd37cf6dc5d5f2b76f7ae1f2c3e314cde
Jayne <jaynegale@gmail.com>
2020-09-19 13:29:53 +1200

    Setting up iActivatables

---
+++ Hexagon Puzzle/Assets/Scripts/Click.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    int swapTri;
+    int changeFactor = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                IActivatable<int> obj = hitInfo.collider.GetComponent<IActivatable<int>>();
+                if (obj != null)
+                {
+//					Player player = hitInfo.collider.GetComponent<Player>();
+//					if (player == null || player.IsDead() == false) // if its not an IDamagable, or if its a destroyed enemy,  or if the player is NOT dead
+                    {
+                        obj.Activate(swapTri, changeFactor);
+                    }
+                }
+            }
+        }
+}
+
+
---
+++ Hexagon Puzzle/Assets/Scripts/IActivatable.cs
@@ -0,0 +1,7 @@
+﻿internal interface IActivatable<T>
+{
+//    when the player activates a tri, SwapTri is the number (1-5) of the other target Tri affected ie tri 1 is North, 2 NE, 3 SE, 4 SW 5 NW NB it can affect itself?
+    T ChangeFactor { get; set; }
+    //the changefactor can be an angle to rotate, an RGB colour amount or a bool flip
+    void Activate(int SwapTri, T ChangeFactor);
+}
